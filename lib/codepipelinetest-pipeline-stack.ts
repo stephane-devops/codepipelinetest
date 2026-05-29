ced75aebd9bd3c7a5aef5ef9a7c6a7a3c7515321
stephanecharron <m9595@vacv.com>
2026-05-29 15:59:50 -0400

    adding app layer

--- lib/codepipelinetest-pipeline-stack.ts
+++ lib/codepipelinetest-pipeline-stack.ts
@@ -2,6 +2,15 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import * as assert from "node:assert";
+import { CodepipelinetestStack } from './codepipelinetest-stack';
+
+// Define a Stage to hold your application stack
+class AppStage extends cdk.Stage {
+  constructor(scope: Construct, id: string, props?: cdk.StageProps) {
+    super(scope, id, props);
+    new CodepipelinetestStack(this, 'AppLayer');
+  }
+}
 
 export class CodepipelinetestPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -9,12 +18,15 @@ export class CodepipelinetestPipelineStack extends cdk.Stack {
 
     const githubRepo = this.node.tryGetContext('github_repo') || assert.fail('github_repo context must be provided');
 
-    new CodePipeline(this, 'Pipeline', {
+    const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'githubtest',
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub(githubRepo, 'main'),
         commands: ['npm ci', 'npm run build', 'npx cdk synth'],
       }),
     });
+
+    // 4. Add the deployment stage to the pipeline
+    pipeline.addStage(new AppStage(this, 'Deploy'));
   }
 }
--- lib/codepipelinetest-stack.ts
+++ lib/codepipelinetest-stack.ts
@@ -1,16 +1,29 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3n from 'aws-cdk-lib/aws-s3-notifications';
 
 export class CodepipelinetestStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // 1. Create the SQS Queue
+    const queue = new sqs.Queue(this, 'MyQueue', {
+      visibilityTimeout: cdk.Duration.seconds(300)
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TmpCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // 2. Create the S3 Bucket
+    const bucket = new s3.Bucket(this, 'MyBucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    // 3. Add S3 Event Notification to SQS
+    // This triggers when any object is created in the bucket
+    bucket.addEventNotification(
+      s3.EventType.OBJECT_CREATED,
+      new s3n.SqsDestination(queue)
+    );
   }
 }
